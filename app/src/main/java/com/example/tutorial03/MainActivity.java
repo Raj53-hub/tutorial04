e7b214d328a2fcbae2db5b62eb7bb9980a3490c6
Raj <rvasoya618@rku.ac.in>
2021-07-25 15:49:43 +0530

    initial commit

---
+++ app/src/main/java/com/example/tutorial03/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.tutorial03;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loginClick(View view) {
+        Toast.makeText(this, "Login Clicked", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, welcome.class);
+        startActivity(intent);
+        finish();
+    }
+}
